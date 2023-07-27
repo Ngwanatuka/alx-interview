ac22cd270a7c750bfce1ad03c288ffe0f1294e5f
Ngwanatuka <ngwanatuka@gmail.com>
2023-07-27 18:57:58 +0200

    adding pascal's traingle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Pascal's Triangle"""
+
+
+def pascal_triangle(n):
+
+    if n <= 0:
+        return []
+
+    traingle = [[1]]
+    for i in range(1, n):
+        prev_row = traingle[i-1]
+        new_row = [1]
+        for j in range(1, i):
+            new_row.append(prev_row[j - 1] + prev_row[j])
+        new_row.append(1)
+        traingle.append(new_row)
+
+    return traingle
