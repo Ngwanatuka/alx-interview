c4ae1c9354ea37d69eb7d8db64efd24133e50bec
Ngwanatuka <ngwanatuka@gmail.com>
2024-02-15 17:35:27 +0200

    Prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Prime Game"""
+
+
+def isWinner(x, nums):
+    def calculate_winner(num, memo):
+        if num < 2:
+            return "Ben"
+
+        if memo[num] is not None:
+            return memo[num]
+
+        for i in range(2, int(num**0.5) + 1):
+            if num % i == 0:
+                if calculate_winner(num // i, memo) == "Ben":
+                    memo[num] = "Maria"
+                    return "Maria"
+
+        memo[num] = "Ben"
+        return "Ben"
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for n in nums:
+        memo = [None] * (n + 1)
+        winner = calculate_winner(n, memo)
+        if winner == "Maria":
+            maria_wins += 1
+        elif winner == "Ben":
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
